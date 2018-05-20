f1d35caeeaebf7778056727e61f0687e2df87764
GRIDI <lin8ro@gmail.com>
2018-05-20 11:36:45 +0300

    Step7
    Add realization TakeBook() for Library.

--- STU-for-Software-Engineering/STU-for-Software-Engineering/Library.cpp
+++ STU-for-Software-Engineering/STU-for-Software-Engineering/Library.cpp
@@ -14,7 +14,38 @@ Library::Library()
 
 void Library::TakeBook()
 {
-	
+	int s = 0;
+	for (int i = 0; i < amount; i++)
+	{
+		if (Library_Foundation[i].Status != 1)
+		{
+			cout << i + 1 << " --- " << Library_Foundation[i].Name << endl;
+			s++;
+		}
+	}
+
+	if (s == 0)
+	{
+		cout << endl << "Новых книг нет! " << endl;
+		system("pause");
+		return;
+	}
+
+	int k;
+	cout << "Введите номер: ";
+	cin >> k;
+	k--;
+
+	if (k<0 || k>s || Library_Foundation[k].Status == 1)
+	{
+		cout << endl << "Неверный номер книги!" << endl;
+	}
+	else
+	{
+		Library_Foundation[k].Status = 1;
+		cout << endl << "Спасибо что взяли книгу " << Library_Foundation[k].Name << endl;
+	}
+	system("pause");
 }
 
 void Library::BackBook()
